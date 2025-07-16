8112c32aad9ae95ddc34b04c710d73b70d487975
samuel nyota karanja <samuel.karanja3@student.moringaschool.com>
2025-07-16 14:53:44 +0300

    complete lab

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+function Header({isDarkMode,onDarkModeClick}){
+   return(
+      <> 
+        <h2>Shopster</h2>
+        <button onClick={onDarkModeClick}>
+          {isDarkMode ? "Dark" : "Light"} Mode
+        </button>
+      </> 
+   ) 
+}
+export default Header
